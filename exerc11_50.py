523ff1b80128b447c99a5e9d2c3755d87206a848
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-04-30 12:31:40 -0300

    aprendendo comando print

---
+++ exerc11_50.py
@@ -0,0 +1,17 @@
+#11. print basico
+print("Aprendendo Algoritmo!")
+
+#12. print em duas linhas
+
+print("Aprendendo Algoritmo!")
+print("Com Scorsese e Coppola!")
+
+#13. print com . \n na segunda linha
+
+print("Aprendendo Algoritmo!")
+print("\nCom Anitta e Ludmila!")
+
+#14. print com . \n na primeira linha
+
+print("Aprendendo Algoritmo!\n")
+print("Com Anitta e Ludmila!")
