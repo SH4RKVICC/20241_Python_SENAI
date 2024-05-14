ba0f28559b8747103f42c49b324581b0ead4ea99
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-14 16:06:08 -0300

    PythonSenaiFJ: Realizando Cálculos03

--- calculos.03.py
+++ calculos.03.py
@@ -25,9 +25,9 @@
 
 #81. criar algoritmo que dado um num. de conta corrente com 3 digitos
 #retornar o seu digito verificador
-d = int(input('Insira seu núm de 3 dígitos: '))
-d1 = round((d / 100) * 1)
-d2 = round((d % 100 / 10) * 2)
-d3 = round((d % 100 % 10) * 3)
-dv = d1 + d2 + d3
-print(f'Digito Verificador: {dv}.')
+# d = int(input('Insira seu núm de 3 dígitos: '))
+# d1 = round((d / 100) * 1)
+# d2 = round((d % 100 / 10) * 2)
+# d3 = round((d % 100 % 10) * 3)
+# dv = d1 + d2 + d3
+# print(f'Digito Verificador: {dv}.')
---
+++ estruturaDeSelecao.py
@@ -0,0 +1,2 @@
+#83 ler núm. e se ele for positivo emitir seu inverso, caso contrario emitir o valor absoluto do núm;
+n = int(input('Insira o núm. inteiro desejado: '))
