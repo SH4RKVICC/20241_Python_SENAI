ee38a4f978f972f3cdaded0b12bccd3e258c2d68
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-14 16:41:10 -0300

    PythonSenaiFJ: Aplicando Estruturas de Seleção!

--- estruturaDeSelecao.py
+++ estruturaDeSelecao.py
@@ -1,2 +1,42 @@
-#83 ler núm. e se ele for positivo emitir seu inverso, caso contrario emitir o valor absoluto do núm;
-n = int(input('Insira o núm. inteiro desejado: '))
+#83 ler núm. e se ele for positivo emitir seu inverso,
+#caso contrario emitir o valor absoluto do núm;
+# n = int(input('Insira o núm. inteiro desejado: '))
+# if (n < 0):
+#   a = abs(n)
+#   print(f'Absoluto: {a}')
+# else:
+#   i = 1 / n
+#   print(f'Inverso: {i}')
+
+#84. ler núm. e emitir se ele é par ou impar
+# n = int(input('Insira o núm. desejado: '))
+# if (n % 2 == 0):
+#   print('O núm. definido é par.')
+# else:
+#   print('O núm. definido é ímpar.')
+
+#85. descubra
+# a = float(input('Insira o primeiro número: '))
+# b = float(input('Insira o segundo número: '))
+# c = float(input('Insira o terceiro número: '))
+# if (a > b):
+#   if (a > c):
+#     max = a
+#   else:
+#     max = c
+# else:
+#   if (b > c):
+#     max = b
+#   else:
+#     max = c
+# print(f'Max: {max:.2f}')
+
+#86.ler se um núm. é positivo, negativo ou nulo
+# n = int(input('Insira o núm. desejado: '))
+# if (n > 0):
+#   print('O número é positivo!')
+# else:
+#   if (n == 0):
+#     print('O número é nulo!')
+#   else:
+#     print('O número é negativo!')
