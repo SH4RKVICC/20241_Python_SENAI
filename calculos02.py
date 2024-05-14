41e1e501dcc3f67cd25cd142c6be3413a6d75b0f
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-14 14:11:34 -0300

    PythonSenaiFJ: Realizando Cálculos

--- calculos02.py
+++ calculos02.py
@@ -86,3 +86,9 @@
 # dq = v1**2 - v2**2
 # print(f'Quadrado da Diferença: {qd}.\nDiferença dos Quadrados: {dq}.')
 
+#78.dado um poligono convexo de n lados, podemos calcular o num. de diagonais
+#desse poligno pela formula nd = n (n - 3) /2. Ler quandos lados tem o poligono,
+#calcular e escrever o num. de diagonais diferentes
+# nl = int(input('Insira o num. de lados do poligono:'))
+# nd = nl * (nl - 3) / 2
+# print(f'Num. de Diagonais: {nd:.0f}.')
