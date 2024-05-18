5d8e46de78a9b6d571298a3683b6fc7d51e90686
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-18 19:40:46 -0300

    PythonSenaiFJ: organizando pastas04

--- AprendendoInput/Algoritmo45.py
+++ AprendendoInput/Algoritmo45.py
@@ -1,6 +1,10 @@
-#45.
-#n = float(input("Inserir o número desejado: "))
-#q =
-#rq =
+#45. ler um número e mostrar raiz quadrada e o quadrado desse numero.
+import math
 
+n = float(input("Inserir o número desejado: "))
+q = n ** 2
+rq = math.sqrt(n)
+print("Número: ", n)
+print("Quadrado: ", q)
+print("Raiz Quadrada: ", rq)
 print("FROM SH4RKVICC")
