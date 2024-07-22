e04aa50debe22b60b553b0625ab36aa0da910dbf
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-07-21 22:42:16 -0300

    python(if-else): implementando exercício 123.

--- AprendendoIfElse/Algoritmo123.py
+++ AprendendoIfElse/Algoritmo123.py
@@ -1,2 +1,18 @@
-# 123. Ler três números, os possíveis lados de um triângulo, e imprimira classificação se-
-# 941 gundo os lados.
+# 123. Ler três números, os possíveis lados de um triângulo, e imprimira classificação
+#  segundo os lados.
+
+a = float(input("Insira o primeiro lado do triângulo: "))
+b = float(input("Insira o segundo lado do triângulo: "))
+c = float(input("Insira o terceiro lado do triângulo: "))
+
+if (a < b + c and b < a + c and c < a + b):
+    if (a == b and a == c):
+        t = 'Equilatero'
+    else:
+        if (a == b or a == c or b == c):
+            t = 'Isoceles'
+        else:
+            t = 'Escaleno'
+    print(f'Triangulo {t}')
+else:
+    print('As medidas não formam um triângulo.')
